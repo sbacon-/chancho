0b3b1210c4e0adb2f1bb34b8710084d82177930b
sbacon- <tchum95@gmail.com>
2019-12-24 15:02:31 +0000

    Merry Christmas

--- styles/scripts.js
+++ styles/scripts.js
@@ -3,3 +3,9 @@ function displayNav(){
   if(element.style.display=="block")element.style.display="none";
   else element.style.display="block";
 }
+function submitSearch(){
+  var searchString = document.getElementById('search-query');
+  var searchString = searchString.value;
+  console.log(searchString);
+  window.open("https://duckduckgo.com/?q="+searchString, '_blank');
+}
