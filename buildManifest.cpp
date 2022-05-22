098c49ba02d27c8612f8fa3c52989833e696292f
Tom Chumley <tom@chancho.dev>
2022-05-21 21:51:38 -0400

    Added programs to split and reconstruct manifest.chancho

---
+++ buildManifest.cpp
@@ -0,0 +1,39 @@
+#include <fstream>
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <algorithm>
+#include <filesystem>
+
+int main(){
+	std::string manifest_path {"manifest.chancho"};
+	std::remove(manifest_path.c_str());
+	std::ofstream outf {manifest_path};
+	std::string folder_path {"manifest"};
+	
+	for(const auto &entry : std::filesystem::directory_iterator(folder_path)){
+		
+		std::string file_path {entry.path()};	
+		std::replace(file_path.begin(),file_path.end(),'-','/');
+		file_path = "---"+file_path.substr(folder_path.size());
+		outf<<file_path<<'\n';
+
+		std::ifstream inf {entry.path()};
+		std::string line;
+		while(inf){
+			std::getline(inf,line);
+			outf<<line<<'\n';
+		}
+
+		outf<<'\n';
+		//Close the files
+		inf.close();
+	}
+
+	outf.close();
+	return 0;
+}
+
+
+
+
