0a49ffd129286f610dc9c7218d177cb9e27ac8dc
sbacon- <tom@chancho.dev>
2023-02-09 11:12:45 -0500

    home page content + style update

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,33 +1,70 @@
 <script>
+	import Fa from 'svelte-fa'
+  import { faGithubAlt } from '@fortawesome/free-brands-svg-icons'
+
+  let updates = [
+  {
+    "date":"02/08/23",
+    "update":"Rebuilding the site in Svelte & TailwindCSS",
+    "link":"https://github.com/sbacon-/chancho/tree/svelte"
+  },
+  {
+    "date":"12/31/22",
+    "update":"Found a use for Amoeba in executing macros in video games, calls for a re-write",
+    "link":"https://github.com/sbacon-/AMOEBA"
+  },
+  {
+    "date":"11/27/22",
+    "update":"CSS Updates, minor changes to Amoeba",
+    "link":"https://github.com/sbacon-/AMOEBA"
+  },
+  {
+    "date":"06/20/22",
+    "update":"Back from Vacaction, refactored gdChessTrainer to use UCI notation",
+    "link":"/proj/gdchesstrainer"
+  },
+  {
+    "date":"06/08/22",
+    "update":"Just completed some consulting work for excel macros",
+    "link":""
+  }
+  ]
+
+  
 
 </script>
 
 <section class = "p-20 text-white">
-<h3 class="text-2xl">Welcome home!</h3>
+<h3 class="text-2xl underline">Welcome Home!</h3>
 
 <br>
-<h3 class="text-2xl">Updates</h3>
+<h3 class="text-center text-xl underline">
+Updates</h3>
+<ul class="flex-col bg-gray-800 border-4 border-white">
+{#each updates as {date,update,link}}
+  <li class="grid grid-cols-6 p-1"> 
+    <h4 class="border-b-2 border-white">{date}</h4>
+    <p class="border-b-2 col-span-4">{update}</p>
+    {#if link!=""}
+      <a class = "border-b-2 text-center" href={link}><Fa class="inline" icon="{faGithubAlt}"/></a>
+    {/if}
+{/each}
+</ul>
+<br>
+<h3 class="text-xl text-center underline">
+Current Project: Rebuild the Site using the new stack.</h3>
 <p>
-11/27/22<br>
-Updated the CSS again, made some changes to the 'Amoeba' java project for repurposing to my new job.<br>
-Github Link: <a href="https://github.com/sbacon-/AMOEBA">AMOEBA</a><br>
-06/20/22<br>
-Getting back from vacation, considering refactoring the gdChess Trainer to better utilize UCI<br>
-06/08/22<br>
-Just completed some consulting work for peer reviewing macros in excel<br>
-</p>
-<p>
+
 <br>
-I've setup a server to host the site locally to allow for POST requests.<br>
-This will allow me to create leader boards, and other PHP functionality.<br>
-Excited to add new features and pages<br>
+Working with a new stack for my website, previously this was mainly Apache
+and a custom HTML build framework I had written in C++.
+<a href="https://github.com/sbacon-/chancho/"><Fa class="inline" icon="{faGithubAlt}"/></a>
+Ultimately this has proved to be too limiting (instant legacy code). So I've decided to make the move to a new stack.
+Currently: Node, Svelte, TailwindCSS, NGINX
+My hope is to use NGINX as a reverse proxy, which will allow me serve more dynamic content through Node.
+Svelte and Tailwind are proving to be incredibly fast and light weight.
 <br>
-I've also created a new C++ program that writes the HTML for me.<br>
-Making it far easier to frequently add new content. So stay tuned.<br>
-Check it out over on <a href="https://github.com/sbacon-/chancho"> gitHub </a>.<br>
-On a semi-related note, the minecraft server is back: <em>mc.chancho.dev</em><br>
-<br>
-Email me your sheets if you'd like to join the queue.<br>
+For those brave enough to join, the minecraft server should still be running: <em>mc.chancho.dev</em><br>
 </p>
 <ul id="library"></ul>
 </section>
