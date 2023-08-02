dca37c548eb25ab2482e654182b09c8335ab01d7
tom <tom@gonzo.attlocal.net>
2023-08-01 23:21:48 -0400

    Added Locale Time & Date or UTC Calculated Date

--- src/routes/apps/Pomo.svelte
+++ src/routes/apps/Pomo.svelte
@@ -1,9 +1,44 @@
 <script>
+	let date = new Date();
+	let time_locale = date.toLocaleTimeString();
+	let date_locale = date.toLocaleDateString();
+	
+	const month_str = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+	let days = [31,28,31,30,31,30,31,31,30,31,30,31];
+	
+	let millis = Date.now();
+	let secs = millis/1000;
+	let min = secs/60;
+	let hour = min/60;
+	let day = hour/24;
+
+	//YEAR
+	let year = 1970;
+	while (day>365){
+		let days_in_year = year%4==0?366:365;
+		day-=days_in_year;
+		year++;
+	}
+	if(year%4==0) days[1] = 29;
+	//MONTH
+	let month = 0;
+	while(day>days[month]){
+		day-=days[month];
+		month++;
+	}
+	//HOUR
+	hour = day*24;
+
+
 </script>
 <div class="w-[90vw] xl:w-[40vw] grow flex flex-col bord m-auto p-3" >
     <h3>Pomodoro<em class="red-txt">!!</em></h3>
+
     <p>
-        {Date.now()}
+	{date_locale}<br>
+	{time_locale} <br><br>
+	{month_str[month] + " " + Math.ceil(day) + ", "+year}<br>
+	{hour}<br>
 
     </p>
 
---
+++ src/routes/apps/Taskr/+page.svelte
@@ -0,0 +1,111 @@
+<script>
+	import Fa from 'svelte-fa';
+	import { onMount } from 'svelte';
+	import { faFileImport } from '@fortawesome/free-solid-svg-icons';
+	import { faGear } from '@fortawesome/free-solid-svg-icons';
+	import { faBomb } from '@fortawesome/free-solid-svg-icons';
+	import { faPencil } from '@fortawesome/free-solid-svg-icons';
+	import { faFloppyDisk } from '@fortawesome/free-solid-svg-icons';
+
+    let widget_name="Todo";
+	let todoText = '';
+	let confirm_dialogue = "Clear";
+
+	let todos = [];
+	onMount(()=>{
+		const existingTodos = localStorage.getItem('todos');
+		todos = JSON.parse(existingTodos) || [];
+	});
+
+    function clickTodo(index){
+        console.log(index);
+        todos=todos.filter(a => a!==index);
+		localStorage.setItem('todos',JSON.stringify(todos));
+    }
+
+    //FETCH
+    function fetchData(){
+    }
+
+    function abortFetch(){
+    }
+
+    //CREATE
+
+	function addTodo(){
+		if(todoText=="")
+			return;
+		todos = [...todos, todoText];
+		localStorage.setItem('todos',JSON.stringify(todos));
+		todoText="";
+	}
+
+    //UPDATE
+    function selectMode(){
+        alert("do something");
+    }
+    function abortGear(){
+    }
+
+    //DELETE
+	function clearAll(){
+		if(confirm()){
+			todos=[];
+			confirm_dialogue="Clear";
+			localStorage.removeItem('todos');
+		}
+	}
+	function confirm(){
+		let confirm_text = "Clear All?";
+		if(confirm_dialogue != confirm_text){
+			confirm_dialogue = confirm_text;
+			return false;
+		}else{
+			return true;
+		}
+	}
+	function abortClear(){
+		confirm_dialogue = "Clear";
+	}
+
+</script>
+
+<div class="grow flex flex-col updates">
+	<div class="w-[90vw] xl:w-[40vw] grow flex flex-col bord m-auto p-3" >
+        <h3 class="text-center underline">{widget_name}<em class="red-txt">!!</em></h3>
+	<!--READ TASKS-->
+	<div class="basis-11/12 flex flex-col grow min-h-[10vh]">
+		<ul>
+            {#each todos as todo}
+                <a on:click|preventDefault={clickTodo(todo)} href="/#">
+					<li class="text-center hover:bg-pri3 dark:hover:bg-pri2dark m-auto text-xl">
+						{todo}
+					</li>
+				</a>
+			{/each}
+		</ul>
+	</div>
+	<!--WRITE TASKS-->
+	<form on:submit|preventDefault={addTodo} class="flex flex-col m-3 border-dotted border-pri3 dark:border-pri2dark border-t-4" action="" >
+		<div class="grow text-xl flex my-3">
+            <a aria-label="update" class="taskFunction flex" href="/#" on:mouseleave={abortGear} on:click|preventDefault={selectMode}>
+                <Fa icon="{faGear}"/>
+                UPDATE
+            </a>
+            <a aria-label="fetch" class="taskFunction flex" href="/#" on:mouseleave={abortFetch} on:click|preventDefault={fetchData}>
+				<Fa icon="{faFileImport}"/>
+                FETCH
+			</a>
+            <a aria-label="delete" class="taskFunction flex" href="/#" on:mouseleave={abortClear} on:click|preventDefault={clearAll}>
+				<Fa icon="{faBomb}"/>
+                DELETE
+			</a>
+		</div>
+        <div class="grow flex p-2 bg-pri3 dark:bg-pri2dark hover:dark:bg-pri2dark duration-1000 rounded-3xl">
+			<Fa class="grow m-auto" icon="{faPencil}"/>
+			<input placeholder="CREATE" bind:value={todoText} type="text" id="task" name="task" class="p-3 bg-pri3 dark:bg-pri2dark border-2 border-text dark:border-textdark rounded-3xl basis-10/12 tranistion-color duration-1000">
+            <Fa class="grow m-auto" icon="{faFloppyDisk}"/>
+		</div>
+	</form>
+	</div>
+</div>
